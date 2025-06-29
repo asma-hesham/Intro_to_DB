963df285dc2e611b94defbecf0caf7679db147c5
asma-hesham <asmaahesham801@gmail.com>
2025-06-29 22:48:24 +0300

    Update alx_book_store.sql
    changes

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,16 +1,12 @@
---Create database
 CREATE DATABASE alx_book_store;
 
--- Use the database
 USE alx_book_store;
 
--- Create Authors table
 CREATE TABLE Authors (
     author_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- Create Books table
 CREATE TABLE Books (
     book_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
@@ -20,7 +16,6 @@ CREATE TABLE Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Create Customers table
 CREATE TABLE Customers (
     customer_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
@@ -28,7 +23,6 @@ CREATE TABLE Customers (
     address TEXT
 );
 
--- Create Orders table
 CREATE TABLE Orders (
     order_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
@@ -36,7 +30,6 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Create Order_Details table
 CREATE TABLE Order_Details (
     orderdetailid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
